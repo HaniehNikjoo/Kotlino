e620b3d6133267549f9a5355fd29a430c9a895d6
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 13:19:10 +0430

    Added IfStatements.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session3/IfStatements.kt
@@ -0,0 +1,20 @@
+package com.example.kotlintutorial.session3
+
+fun main(args: Array<String>) {
+
+    var isWorkingConditionExcellent = true
+
+    if (isWorkingConditionExcellent){
+
+        //Lines of code
+        println("Excellent")
+    }
+
+    var isWorkingConditionPoor = false
+
+    if (!isWorkingConditionPoor){
+
+        print("Poor")
+    }
+
+}
