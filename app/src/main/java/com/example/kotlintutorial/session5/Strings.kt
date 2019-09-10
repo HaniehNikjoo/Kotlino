9fb528cb5a9c6bc10704644b94063aaf18287711
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 17:06:41 +0430

    Added Strings.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session5/Strings.kt
@@ -0,0 +1,18 @@
+package com.example.kotlintutorial.session5
+
+fun main(args: Array<String>) {
+
+    var myAnimalName : String = "My Lion"
+    println(myAnimalName)
+    println(myAnimalName.toLowerCase())
+    println(myAnimalName.toUpperCase())
+    println(myAnimalName.split("M"))
+    println(myAnimalName.split(" "))
+    println(myAnimalName[0])
+    println(myAnimalName[1])
+    println(myAnimalName[2])
+    println(myAnimalName[3])
+    println(myAnimalName[4])
+    println(myAnimalName[5])
+    println(myAnimalName[6])
+}
