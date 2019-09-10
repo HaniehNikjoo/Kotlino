400b584f97a29b0abca739412bdc3559f214d0eb
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 14:10:54 +0430

    Added IfElseIfStatement.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session3/IfElseIfStatement.kt
@@ -0,0 +1,21 @@
+package com.example.kotlintutorial.session3
+
+fun main(args: Array<String>) {
+    /* if (condition) {
+       statement
+    }else if (condition) {{
+       statement
+    }else{
+       statement
+    }
+    */
+
+    var score = 200
+    if(score > 300)
+        println("Great score")
+    else if (score > 150)
+        println("Excellent score")
+    else if (score < 10)
+        println("Bad score!")
+
+}
