c9d36b86035d2f3400d671204c6c6d11fc412828
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 11:39:11 +0430

    AddeD OperationPrecedence.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session1/OperationPrecedence.kt
@@ -0,0 +1,24 @@
+package com.example.kotlintutorial.session1
+
+fun main(args: Array<String>) {
+
+    /*
+    Priority
+    ()
+    ^
+    *,/
+    * +,-
+     */
+
+    var number = 2 * 3 + 1/7 * Math.pow(2.0,3.0) - (3 - 1)
+    print(number)
+
+    //6
+    //0
+    //2 * 2 * 2 = 8
+    //0 * 8 = 0
+    //6 + 0 = 6
+    //3 - 1 = 2
+    //6 - 2 = 4.0
+
+}
