18e079f5ce4bc3ceb9269b5c9a93e393bd81d574
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 20:28:08 +0430

    Added HashmapOf.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session5/HashmapOf.kt
@@ -0,0 +1,8 @@
+package com.example.kotlintutorial.session5
+
+fun main(args: Array<String>) {
+
+    var dictionary = hashMapOf<String,String>("hello" to "Salutation","nice" to "Beaufiful")
+    println(dictionary["hello"])
+
+}
