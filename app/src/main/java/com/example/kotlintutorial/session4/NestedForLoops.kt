7ff6a728d43b41251ec9eecb5ded09c710176e35
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 15:57:34 +0430

    Updated NestedForLoops.kt

--- app/src/main/java/com/example/kotlintutorial/session4/NestedForLoops.kt
+++ app/src/main/java/com/example/kotlintutorial/session4/NestedForLoops.kt
@@ -2,4 +2,10 @@ package com.example.kotlintutorial.session4
 
 fun main(args: Array<String>) {
 
+    for(myNumber in 1..5) {
+        println("$myNumber . OutSideForLoop")
+        for (mySecondNumber in 1..3)
+            println("$mySecondNumber - InSideForLoop")
+    }
+
 }
