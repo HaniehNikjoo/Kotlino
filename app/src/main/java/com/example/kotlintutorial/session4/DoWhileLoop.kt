052764a1c32d9431f594509ff194a860bdaa9c1a
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 16:27:54 +0430

    Added DoWhileLoop.kt

--- app/src/main/java/com/example/kotlintutorial/session4/DoWhileLoop.kt
+++ app/src/main/java/com/example/kotlintutorial/session4/DoWhileLoop.kt
@@ -1,2 +1,15 @@
 package com.example.kotlintutorial.session4
 
+fun main(args: Array<String>) {
+
+//    var peopleAreFriendly : Boolean = true
+    var peopleAreFriendly : Boolean = false
+    var counter = 0
+
+    do {
+        println("Talk to people")
+        counter++
+
+    }while (peopleAreFriendly && counter < 5)
+
+}
