67fae9bf90ba3d40be160e046dc8391171e5b5a6
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 15:49:50 +0430

    Added ForLoop.kt and NestedForLoops.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session4/ForLoop.kt
@@ -0,0 +1,7 @@
+package com.example.kotlintutorial.session4
+
+fun main(args: Array<String>) {
+
+    for(myNumber in 1..10)
+        println("num - $myNumber")
+}
---
+++ app/src/main/java/com/example/kotlintutorial/session4/NestedForLoops.kt
@@ -0,0 +1,5 @@
+package com.example.kotlintutorial.session4
+
+fun main(args: Array<String>) {
+
+}
