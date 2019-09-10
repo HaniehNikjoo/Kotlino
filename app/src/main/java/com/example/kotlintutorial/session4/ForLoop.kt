07b1b10c755ba6c7b4e268ea27e7d6d76e7aacb1
HaniehNikjoo <hnik223@gmail.com>
2019-09-10 18:20:33 +0430

    Added examples in Arrays.kt and ForLoop.kt

--- app/src/main/java/com/example/kotlintutorial/session4/ForLoop.kt
+++ app/src/main/java/com/example/kotlintutorial/session4/ForLoop.kt
@@ -4,4 +4,13 @@ fun main(args: Array<String>) {
 
     for(myNumber in 1..10)
         println("num - $myNumber")
+
+    // For loop
+    for (i in 0..10) {
+        println(i)
+    }
+    // ForEach
+    (0..10).forEach {
+        println(it)
+    }
 }
--- app/src/main/java/com/example/kotlintutorial/session5/Arrayst.kt
+++ app/src/main/java/com/example/kotlintutorial/session5/Arrayst.kt
@@ -3,18 +3,44 @@ package com.example.kotlintutorial.session5
 fun main(args: Array<String>) {
 
     //var name = Array<T>(size){defaultValue}
-    var partNames = Array<String>(10){"*"}
-    partNames[0] = "John"
-    println(partNames[0])
-    partNames[1] = "Liza"
-    println(partNames[1])
-    partNames[9] = "Hana"
-    println(partNames[9])
+    var partyNames = Array<String>(10){"*"}
+    partyNames[0] = "John"
+    println(partyNames[0])
+    partyNames[1] = "Liza"
+    println(partyNames[1])
+    partyNames[9] = "Hana"
+    println(partyNames[9])
 
     println("------------------")
 
-    for (myVar in partNames){
+    // ForEach
+    for (myVar in partyNames){
         println(myVar)
+    }
+    // ForEach
+    (partyNames).forEach {
+        println("TAG:"+it)
+    }
+
+    println("All party names by using index")
+    partyNames[2] = "A"
+    partyNames[3] = "B"
+    partyNames[4] = "C"
+    partyNames[5] = "D"
+    partyNames[6] = "E"
+    partyNames[7] = "F"
+    partyNames[8] = "G"
+    partyNames[9] = "H"
+
+    for (myVar in 0..9)
+        println(partyNames[myVar])
+
+    println()
+    println("All party names invited by the user")
+
+    for (myVar in 0..9) {
+        partyNames[myVar] = readLine()!!
+        println(" "+partyNames[myVar]+" is invited to the party.")
     }
 
 }
