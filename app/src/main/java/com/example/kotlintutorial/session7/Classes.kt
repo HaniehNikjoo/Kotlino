248af7c0acd8e8e6e9cc21d3ca2b1302f6182ea8
HaniehNikjoo <hnik223@gmail.com>
2019-09-12 09:42:18 +0430

    Added Classes.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session7/Classes.kt
@@ -0,0 +1,11 @@
+package com.example.kotlintutorial.session7
+
+class Car(){
+
+    //Properties in the class must be initialized
+    var speed: Int = 0
+    var power: Int = 0
+    var name: String = ""
+    var nmberOfWheels:Int = 0
+
+}
