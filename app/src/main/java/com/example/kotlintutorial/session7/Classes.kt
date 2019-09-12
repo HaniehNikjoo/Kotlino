846361daa47daa25f62139e2f29af2a6093e5344
HaniehNikjoo <hnik223@gmail.com>
2019-09-12 10:13:35 +0430

    Updated Classes.kt

--- app/src/main/java/com/example/kotlintutorial/session7/Classes.kt
+++ app/src/main/java/com/example/kotlintutorial/session7/Classes.kt
@@ -1,5 +1,21 @@
 package com.example.kotlintutorial.session7
 
+fun main(args: Array<String>) {
+
+    //Create object from class
+    var myCar: Car = Car()
+    println(myCar.speed)
+    println(myCar.power)
+
+    var yourcar: Car = Car()
+    yourcar.speed=200
+    println(yourcar.speed)
+    println(myCar.speed)
+
+    println(Car().nmberOfWheels)
+
+}
+
 class Car(){
 
     //Properties in the class must be initialized
