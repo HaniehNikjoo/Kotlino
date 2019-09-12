9e71c03840ac0ec08ea853ba2e4b897a60f741e8
HaniehNikjoo <hnik223@gmail.com>
2019-09-12 11:41:17 +0430

    Added CustomConstructors.kt

---
+++ app/src/main/java/com/example/kotlintutorial/session7/CustomConstructors.kt
@@ -0,0 +1,26 @@
+package com.example.kotlintutorial.session7
+
+fun main(args: Array<String>) {
+
+    var animal = Animal("My Lion","Yellow",2000,4000)
+    var myPanther = Animal(5000)
+
+}
+
+class Animal(speed: Int){
+    init {
+        println(speed)
+    }
+
+    /*
+    Secondary constructors
+    The class can also declare secondary constructors, which are prefixed with constructor
+    If the class has a primary constructor, each secondary constructor needs to delegate to the primary constructor,
+    either directly or indirectly through another secondary constructor(s).
+    Delegation to another constructor of the same class is done using the this keyword
+     */
+    constructor(name: String,color: String,power:Int,speed:Int): this(speed){
+        println(name +" - "+color +" - "+ power +" - "+ speed)
+    }
+
+}
